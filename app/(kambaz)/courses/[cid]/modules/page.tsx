24889189de5d817b4cffd190ba5b9cc6e8ef4960
Jolin Yang <yang.jol@northeastern.edu>
2026-01-25 19:20:31 -0500

    fixed bullet point issue in Modules page

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -14,11 +14,15 @@ export default function Modules() {
                   <li className="wd-content-item">Introduction to the course</li>
                   <li className="wd-content-item">Learn what is Web Development</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">READING</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
                   <li className="wd-content-item">Full Stack Developer - Chapter 1 - Creating User Interfaces with HTML</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">SLIDES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Introduction to Web Development</li>
@@ -31,16 +35,20 @@ export default function Modules() {
           <li className="wd-module">
             <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
             <ul className="wd-lessons">
-              <li className = "wd-lessons">
+              <li className = "wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
                   <li className="wd-content-item">Deploy the assignment to Vercel</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">READING</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Full Stack Developer - Chapter 1 - Creating User Interfaces with HTML</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">SLIDES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Introduction to HTML and the DOM</li>
@@ -56,17 +64,21 @@ export default function Modules() {
           <li className="wd-module">
             <div className="wd-title">Week 2, Lecture 3 - Getting Started with the Kambaz Web Application</div>
             <ul className="wd-lessons">
-              <li className = "wd-lessons">
+              <li className = "wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
                   <li className="wd-content-item">Start implementing the Kambaz Web Application</li>
                   <li className="wd-content-item">Update Vercel Deployment</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">READING</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Full Stack Developer - Chapter 1 - Creating User Interfaces with HTML</li>
                 </ul>
+              </li>
+              <li className="wd-lesson">
                 <span className="wd-title">SLIDES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Implementing the Kambaz Account Screens</li>
