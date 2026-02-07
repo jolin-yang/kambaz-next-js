1b5515c96f8ecf2fb249dfef6690306cb06ceaa4
Jolin Yang <yang.jol@northeastern.edu>
2026-02-07 18:44:14 -0500

    style account screens

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 export default function AccountNavigation() {
  return (
-   <div id="wd-account-navigation">
-     <Link href="signin"> Signin </Link> <br />
-     <Link href="signup"> Signup </Link> <br />
-     <Link href="profile"> Profile </Link> <br />
+   <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0 pt-2">
+     <Link href="signin" id="wd-signin-link"
+        className="list-group-item active border-0"> Signin </Link>
+     <Link href="signup" id="wd-signup-link"
+        className="list-group-item text-danger border-0"> Signup </Link>
+     <Link href="profile" id="wd-profile-link"
+      className="list-group-item text-danger border-0"> Profile </Link>
    </div>
 );}
-
