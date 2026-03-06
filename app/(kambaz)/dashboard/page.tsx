b37180b72094ee83786756ad0c187a3f5c0a9fd3
Jolin Yang <yang.jol@northeastern.edu>
2026-03-06 01:20:01 -0500

    add new changes

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,33 +1,60 @@
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
 import * as db from "../database";
-import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText, Button } from "react-bootstrap";
+import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText, Button, FormControl } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Dashboard() {
- const courses = db.courses;
- return (
-  <div id="wd-dashboard">
-   <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-   <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
-   <div id="wd-dashboard-courses">
-    <Row xs={1} md={5} className="g-4">
-     {courses.map((course) => (
-     <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-      <Card>
-       <Link href={`/courses/${course._id}/home`}
-        className="wd-dashboard-course-link text-decoration-none text-dark" >
-        <CardImg src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
-        <CardBody className="card-body">
-         <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-          {course.name} </CardTitle>
-         <CardText className="wd-dashboard-course-description overflow-hidden" style={{height:"100px"}}>
-          {course.description} </CardText>
-         <Button variant="primary"> Go </Button>
-        </CardBody>
-       </Link>
-      </Card>
-     </Col>
-    ))}
-   </Row>
-  </div>
- </div>);}
+    const [courses, setCourses] = useState<any[]>(db.courses);
+
+    const [course, setCourse] = useState<any>({
+        _id: "0", name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+        image: "/images/reactjs.jpg", description: "New Description"
+      });    
+
+    const addNewCourse = () => {
+        const newCourse = { ...course, _id: uuidv4() };
+        setCourses([...courses, newCourse ]);
+    };
+    
+    return (
+    <div id="wd-dashboard">
+        <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+        <h5 className="fs-3 mb-4">New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={addNewCourse} > Add </button>
+        </h5>
+      <FormControl value={course.name} className="mb-2"
+            onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <FormControl as="textarea" value={course.description} rows={3}
+            onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
+      <hr />
+
+        <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+        <div id="wd-dashboard-courses">
+            <Row xs={1} md={5} className="g-4">
+            {courses.map((course) => (
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+            <Link href={`/courses/${course._id}/home`}
+                className="wd-dashboard-course-link text-decoration-none text-dark" >
+                <CardImg src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
+                <CardBody className="card-body">
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                {course.name} </CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height:"100px"}}>
+                {course.description} </CardText>
+                <Button variant="primary"> Go </Button>
+                </CardBody>
+            </Link>
+            </Card>
+            </Col>
+            ))}
+        </Row>
+        </div>
+    </div>);}
 
