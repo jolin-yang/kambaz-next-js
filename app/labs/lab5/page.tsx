5c3ee51787e01e72b40c60f43add60f85c3cbf45
Jolin Yang <yang.jol@northeastern.edu>
2026-03-27 01:23:35 -0400

    finish lab 5

--- app/labs/lab5/page.tsx
+++ app/labs/lab5/page.tsx
@@ -1,5 +1,12 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
 export default function Lab5() {
@@ -13,6 +20,12 @@ export default function Lab5() {
       </div><hr />
       <EnvironmentVariables />
       <PathParameters/>
+      <QueryParameters/>
+      <WorkingWithObjects/>
+      <WorkingWithArrays/>
+      <HttpClient/>
+      <WorkingWithObjectsAsynchronously/>
+      <WorkingWithArraysAsynchronously/>
     </div>
 );}  
   
