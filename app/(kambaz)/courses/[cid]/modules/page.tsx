40f06dd9c77f69b7c2bc90631f1887443cba6ec6
Jolin Yang <yang.jol@northeastern.edu>
2026-04-09 22:33:42 -0400

    finish modules

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -28,18 +28,21 @@ export default function Modules() {
     const newModule = { name: moduleName, course: cid };
     const module = await client.createModuleForCourse(cid as string, newModule);
     dispatch(setModules([...modules, module]));
+    setModuleName("");
   };
 
   const onRemoveModule = async (moduleId: string) => {
-    await client.deleteModule(moduleId);
+    await client.deleteModule(cid, moduleId);
     dispatch(setModules(modules.filter((m: any) => m._id !== moduleId)));
-  };
+  }; 
 
   const onUpdateModule = async (module: any) => {
-    await client.updateModule(module);
-    const newModules = modules.map((m: any) => m._id === module._id ? module : m );
+    await client.updateModule(cid, module);
+    const newModules = modules.map((m: any) =>
+      m._id === module._id ? module : m
+    );
     dispatch(setModules(newModules));
-  };
+  }; 
 
   useEffect(() => {
     fetchModules();
