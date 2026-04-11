01bffa225d7785aade57206ccc279ca3361ec115
Jolin Yang <yang.jol@northeastern.edu>
2026-04-10 22:05:11 -0400

    update TOC

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -39,7 +39,7 @@ export default function TOC() {
        <NavLink href="https://github.com/jolin-yang/kambaz-next-js/tree/a5" id="wd-github">My GitHub</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jolin-yang/kambaz-node-server-app/tree/a5" id="wd-github">Server repo</NavLink>
+       <NavLink href="https://github.com/jolin-yang/kambaz-node-server-app/tree/a6" id="wd-github">Server repo</NavLink>
      </NavItem><NavItem>
        <NavLink href="https://kambaz-node-server-app-ohyp.onrender.com" id="wd-github">Root of server</NavLink>
      </NavItem>
