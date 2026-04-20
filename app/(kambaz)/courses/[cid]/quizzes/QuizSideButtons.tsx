c3d8810fdf95ddf410aa5f2d8c8925f3248ab020
Jolin Yang <yang.jol@northeastern.edu>
2026-04-19 20:11:46 -0400

    implement publish changes to sidebar

--- app/(kambaz)/courses/[cid]/quizzes/QuizSideButtons.tsx
+++ app/(kambaz)/courses/[cid]/quizzes/QuizSideButtons.tsx
@@ -1,12 +1,16 @@
+"use client";
+
+import * as client from "./client";
 import { IoEllipsisVertical } from "react-icons/io5";
 import { FaCheckCircle } from "react-icons/fa";
 import QuizDeleteDialog from "./QuizDeleteDialog";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { FaPencil, FaTrash } from "react-icons/fa6";
+import { FaBan, FaCheck, FaPencil, FaTrash } from "react-icons/fa6";
+import { updateQuiz } from "./reducer";
 
 export default function QuizSideButtons(
   { quizId, quizName, deleteQuiz }:
@@ -16,23 +20,28 @@ export default function QuizSideButtons(
    const handleShow = () => setShow(true);
    const { currentUser } = useSelector((state: RootState) => state.accountReducer);
    const isFaculty = currentUser?.role !== "STUDENT";
-   const [quizContextMenu, setQuizContextMenu] = useState<string | null>(null);
+   const [showMenu, setShowMenu] = useState(false);
    const { cid } = useParams();
 
-   const openMenu = (quizId: string) => {
-        setQuizContextMenu(quizId);
+   const quiz = useSelector((state: RootState) => state.quizzesReducer.quizzes.find(
+    (q : any) => q._id === quizId)) as any;    
+   const [isPublished, setIsPublished] = useState(quiz?.published);
+
+   const dispatch = useDispatch();
+
+   
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
     };
 
-    const closeMenu = () => {
-        setQuizContextMenu(null);
-    };
-
-    const toggleMenu = (quizId: string) => {
-        if (quizContextMenu === quizId) {
-            closeMenu();
-        } else {
-            openMenu(quizId);
-        }
+    const togglePublishState = async () => {
+        const updatedQuiz = {
+          ...quiz, published: !isPublished
+        };
+        
+        await client.updateQuiz(updatedQuiz);
+        dispatch(updateQuiz(updatedQuiz));  
+        setIsPublished(!isPublished);
     };
   
   
@@ -44,28 +53,37 @@ export default function QuizSideButtons(
             <FaCheckCircle className="text-success fs-3" />
           </span>
     
-      <IoEllipsisVertical className="fs-3" onClick={() => toggleMenu(quizId)} />
+      <IoEllipsisVertical className="fs-3" onClick={() => toggleMenu()} />
       <QuizDeleteDialog show={show} handleClose={handleClose} 
             quizName={quizName} deleteQuiz={deleteQuiz} />
 
-        {quizContextMenu === quizId && (
-        <div className="bg-white border rounded-1 position-absolute ms-4 hover:bg-light z-3">
-            <Link href={`/courses/${cid}/quizzes/${quizId}`} className="d-flex justify-content-center text-black">
-                {/* <FaPencil/>     */}
+        {showMenu && (
+        <div className="bg-white border rounded-1 position-absolute ms-2 hover:bg-light z-3">
+            <Link href={`/courses/${cid}/quizzes/${quizId}`} className="d-flex justify-content-center text-black fs-6">
+                <FaPencil size={15} className="me-1  mt-1"/>
                 Edit
             </Link>
             <div onClick={handleShow} 
-                className="text-blue underline dropdown-item text-center text-black text-decoration-underline"
+                className="text-blue underline dropdown-item text-center text-black text-decoration-underline fs-6"
                 style={{ cursor: "pointer" }}>
-                {/* <FaTrash /> */}
+                <FaTrash size={15} className="me-1"/>
                 Delete
             </div>
-            <Link href={`/courses/${cid}/quizzes`}
-                className="text-blue underline dropdown-item text-center text-black text-primary text-decoration-underline"
+            <div onClick={() => togglePublishState()}
+                className="text-blue underline dropdown-item text-center text-black text-primary text-decoration-underline fs-6"
                 style={{ cursor: "pointer" }}>
-                {/* <FaCheckCircle /> */}
-                Publish
-            </Link>
+                {isPublished ? 
+                    <span>
+                        <FaBan size={15} className="me-1"/> 
+                        Unpublish
+                    </span>
+                : 
+                    <span>
+                        <FaCheck size={15} className="me-1"/> 
+                        Publish
+                    </span> 
+                }
+            </div>
         </div>
         )}
   
